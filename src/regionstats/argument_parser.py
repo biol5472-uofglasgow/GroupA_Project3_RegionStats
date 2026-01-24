c22a2980ce15e9980fac93c2a6dfc62c8d9c13e6
lohithv1999-cell <lohithv1999@gmail.com>
2026-01-24 09:47:23 +0000

    Update argument_parser.py

--- src/regionstats/argument_parser.py
+++ src/regionstats/argument_parser.py
@@ -23,8 +23,16 @@ def build_parser() -> argparse.Namespace:
     #Allows the user to choose between two options for the method to calculate the GC content 
     parser.add_argument("--gc-mode",choices=["include_n", "exclude_n"],default="include_n",help=("GC calculation modes: "
             "'include_n' counts the Ns in the denominator; "
-            "'exclude_n' ignores the Ns in the denominator"
-        )
-    )
+            "'exclude_n' ignores the Ns in the denominator"))
+
+    # Enables bedGraph output
+    parser.add_argument( "--bedgraph", action="store_true", help="Write bedGraph output")
+
+    # Enables BigWig output
+    parser.add_argument( "--bigwig", action="store_true", help="Convert bedGraph to BigWig")
+
+    # Chromosome sizes file (required for BigWig)
+    parser.add_argument( "--chrom-sizes", help="Chromosome sizes file (required for BigWig output)")
 
     return parser.parse_args()
+
