c587d41a2505788d90ff6afd28ea98218d5c82ea
lohithv1999-cell <lohithv1999@gmail.com>
2026-01-28 10:09:42 +0000

    Update __init__.py

--- src/regionstats/__init__.py
+++ src/regionstats/__init__.py
@@ -2,10 +2,10 @@
 
 __version__ = "0.1.0"
 
-from .bedgraph_bigwig_output import bedgraph_to_bigwig, write_bedgraph
-from .fetch_sequence import fetch_sequence, open_fasta
+from .bedgraph_bigwig_output import bedgraph_to_bigwig,write_bedgraph
+from .fetch_sequence import fetch_sequence,open_fasta
 from .interval_handler import load_intervals
-from .region_metrics import gc_fraction, n_fraction, sequence_length
+from .region_metrics import gc_fraction, n_fraction,sequence_length
 from .validation import (
     ValidationError,
     validate_bed,
